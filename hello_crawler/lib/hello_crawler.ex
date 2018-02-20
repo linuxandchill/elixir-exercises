311c316611444a4a89d42e3dece86e9e900228f2
linuxandchill <rivercodi@gmail.com>
2018-02-19 19:09:29 -0800

    crawler

---
+++ hello_crawler/lib/hello_crawler.ex
@@ -0,0 +1,36 @@
+defmodule HelloCrawler do
+
+  def get_links(url) do 
+    with {:ok, %{body: body}} <- HTTPoison.get(url, [], [follow_redirect: true]),
+      tags <- Floki.find(body, "a"),
+      hrefs <- Floki.attribute(tags, "href") do 
+        [url | hrefs]
+    else 
+      _ -> [url]
+
+    end
+
+  end
+"""
+def handle_response({:ok, %{body: body}}, url) do 
+[url | body 
+|> Floki.find("a")
+|> Floki.attribute("href")
+] 
+end
+
+def handle_response(_response,url) do 
+[url]
+end
+
+def get_links(url) do 
+headers = []
+options = [follow_redirect: true]
+
+url
+|> HTTPoison.get(headers, options)
+|> handle_response(url)
+end
+"""
+
+end
